ba9384592d84cec0172336beca05e2ce35409a58
Noah Sheldon <noahsheldon06@gmail.com>
2023-11-18 00:48:47 +0000

    fix navbar, homepage half done

---
+++ shuttle-swaggers/src/app/components/sessions/sessions.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sessions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sessions.component.html',
+  styleUrl: './sessions.component.scss'
+})
+export class SessionsComponent {
+
+}
