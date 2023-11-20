4808fc5db5b224a9143c2b81c091cb9431fe4579
Noah Sheldon <noahsheldon06@gmail.com>
2023-11-20 23:09:51 +0000

    Navbar and homepage fixed

--- shuttle-swaggers/src/app/app.routes.ts
+++ shuttle-swaggers/src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { SessionsComponent } from './components/sessions/sessions.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path: '' , pathMatch:'full', component: HomeComponent},
+    {path: '' , redirectTo:'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'signup', component: SignUpComponent},
+    {path: 'login', component: LoginComponent},
     {path: 'sessions', component: SessionsComponent},
+    {path: '**', component: PageNotFoundComponent}
 ];
