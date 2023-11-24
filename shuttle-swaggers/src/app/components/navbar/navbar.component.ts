7ef4c88ff7c0b2dea9b5ddd92b02aee102c4c33c
Noah Sheldon <noahsheldon06@gmail.com>
2023-11-24 19:38:26 +0000

    changes to home page

--- shuttle-swaggers/src/app/components/navbar/navbar.component.ts
+++ shuttle-swaggers/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
