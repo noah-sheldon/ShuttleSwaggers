ee6f64efce05c886ae66aa2238878048d12190c4
Noah Sheldon <noahsheldon06@gmail.com>
2023-11-25 01:50:10 +0000

    Fixed navbar and homepage

--- shuttle-swaggers/src/app/app.component.ts
+++ shuttle-swaggers/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
