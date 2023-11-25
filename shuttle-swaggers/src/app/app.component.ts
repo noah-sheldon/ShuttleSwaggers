a489159144ce2775d0d33e9fbd89cfd86ba1e268
Noah Sheldon <noahsheldon06@gmail.com>
2023-11-25 12:39:24 +0000

    Navbar created into a component

--- shuttle-swaggers/src/app/app.component.ts
+++ shuttle-swaggers/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { provideRouter } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
