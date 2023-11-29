ce1cda66b301bfa2d4510c10145d3611f785795a
Noah Sheldon <noahsheldon06@gmail.com>
2023-11-29 23:06:37 +0000

    fixed home page, sessions route initial work

--- shuttle-swaggers/src/app/components/sessions/sessions.component.ts
+++ shuttle-swaggers/src/app/components/sessions/sessions.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Player } from '../../models/player';
+import { MatTableModule } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-sessions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './sessions.component.html',
   styleUrl: './sessions.component.scss'
 })
-export class SessionsComponent {
+export class SessionsComponent{
+  constructor(){}
 
+  displayedColumns = ["name"];
+  players : Player[] = [
+    {name: "Noah"},
+    {name: "Amit"},
+    {name: "Sunil"},
+  ];
 }
